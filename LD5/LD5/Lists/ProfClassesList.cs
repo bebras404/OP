686194842e66a0df90945be89fdbb7b11eef9b4f
bebras404 <adodan@ktu.lt>
2025-05-24 23:29:05 +0300

    Jesus

--- LD5/LD5/Lists/ProfClassesList.cs
+++ LD5/LD5/Lists/ProfClassesList.cs
@@ -7,5 +7,17 @@ namespace LD5.Lists
 {
 	public class ProfClassesList
 	{
-	}
+		private List<Student> Classes;
+		private string ClassName { get; set; }
+
+		public ProfClassesList(string className, List<Student> afterFilter) 
+		{
+            Classes = afterFilter;
+            this.ClassName = className;
+        }
+
+
+
+
+    }
 }
